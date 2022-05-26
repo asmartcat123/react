6287c616560b9ed705a8f8c49816b9c30f7a5964
cc <123>
2022-05-26 15:16:45 +0800

    second test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, {useState,useEffect} from "react";
+import {BrowserRouter, Link, Navigate, Route, Routes} from "react-router-dom";
+import Load from "./index/Load";
 import './App.css';
-
+import User from "./index/User";
+import Test from "./index/Test";
+import Login from "./index/Login";
 function App() {
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+          <div >
+            <Routes>
+                <Route path={"/"} element={<Login/>} ></Route>
+                <Route path={"/Load"} element={<Load/>}>
+                    <Route path={"User"} element={<User/>}></Route>
+                </Route>
+                <Route path={"/Test"} element={<Test/>}></Route>
+
+            </Routes>
     </div>
+      </BrowserRouter>
   );
+
+
 }
 
 export default App;
